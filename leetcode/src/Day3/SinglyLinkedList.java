37d892e2285353121300e566aea3b668398d7b65
vaibhavj <vaibhavj@cdac.in>
2023-01-19 12:10:38 +0530

    Singly Linked list

---
+++ leetcode/src/Day3/SinglyLinkedList.java
@@ -0,0 +1,129 @@
+package Day3;
+
+public class SinglyLinkedList {
+	static class Node {
+		private int data;
+		private Node next;
+
+		public Node() {
+			this.data = 0;
+			this.next = null;
+		}
+
+		public Node(int val) {
+			this.data = val;
+			this.next = null;
+		}
+	}
+
+	private Node head;
+
+	public SinglyLinkedList() {
+		this.head = null;
+	}
+
+	public void display() {
+		Node trav = head;
+		while (trav != null) {
+			System.out.println(trav.data);
+			trav = trav.next;
+		}
+	}
+
+	public void addFirst(int val) {
+		Node newNode = new Node(val);
+		if (head == null)
+			head = newNode;
+		else {
+			newNode.next = head;
+			head = newNode;
+		}
+	}
+
+	public void addLast(int val) {
+		Node newNode = new Node(val);
+		Node trav = this.head;
+		if (head == null)
+			head = newNode;
+		else {
+			while (trav.next != null)
+				trav = trav.next;
+			trav.next = newNode;
+		}
+	}
+
+	public void addAtPos(int val, int pos) {
+		if (head == null || pos <= 1)
+			addFirst(val);
+		else {
+			Node newNode = new Node(val);
+			Node trav = this.head;
+			for (int i = 1; i < pos - 1; i++) {
+				if (trav.next == null)
+					break;
+				trav = trav.next;
+			}
+			newNode.next = trav.next;
+			trav.next = newNode;
+		}
+	}
+
+	public void delFirst() {
+		if (head == null)
+			throw new RuntimeException("List is empty");
+		head = head.next;
+	}
+
+	public void delAll() {
+		head = null;
+	}
+
+	public void delAtPos(int pos) {
+		if (pos == 1)
+			delFirst();
+		else if (head == null || pos < 1) {
+			throw new RuntimeException("Empty list");
+		} else {
+			Node trav = this.head;
+			Node temp = null;
+			for (int i = 1; i < pos; i++) {
+				if (trav.next == null)
+					throw new RuntimeException("Invalid position");
+				temp = trav;
+				trav = trav.next;
+			}
+			temp.next = trav.next;
+		}
+	}
+
+	public void delLast() {
+		if (head == null) {
+			throw new RuntimeException("Empty list");
+		} else if (head.next == null) {
+			head = null;
+		} else {
+			Node trav = this.head;
+			Node temp = null;
+
+			while (trav.next != null) {
+				temp = trav;
+				trav = trav.next;
+			}
+			temp.next = trav.next;
+		}
+	}
+
+	public static void main(String[] args) {
+		SinglyLinkedList list = new SinglyLinkedList();
+		list.addFirst(11);
+		list.addLast(21);
+		list.addAtPos(100, 3);
+		list.addLast(27);
+//		list.delFirst();
+//		list.delAll();
+//		list.delAtPos(4);
+		list.delLast();
+		list.display();
+
+	}
+}
