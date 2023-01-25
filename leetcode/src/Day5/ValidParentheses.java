fa14d5075ea9cf65947b6eec2b9b5c81ca901afd
vaibhavj <vaibhavj@cdac.in>
2023-01-26 00:22:48 +0530

    Valid parentheses

---
+++ leetcode/src/Day5/ValidParentheses.java
@@ -0,0 +1,29 @@
+package Day5;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+	public static void main(String[] args) {
+		System.out.println(validity("({{}})"));
+	}
+
+	public static boolean validity(String s) {
+		Stack<Character> stack = new Stack<Character>();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c == '(' || c == '[' || c == '{') {
+				stack.push(c);
+			} else if (c == ')' || c == ']' || c == '}') {
+				if (stack.isEmpty()) {
+					return false;
+				}
+				char top = stack.pop();
+				if ((c == ')' && top != '(') || (c == ']' && top != '[') || (c == '}' && top != '{')) {
+					return false;
+				}
+			}
+		}
+		return stack.isEmpty();
+	}
+}
