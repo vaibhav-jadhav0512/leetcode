654ae087dd76d12d1f94cd56f72e43c6d557b981
Vaibhav <vaibhav-jadhav0512>
2023-07-14 17:59:30 +0530

    RunningSumOf1DArray

---
+++ leetcode/src/leetcode/_6_RunningSumOf1DArray.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class _6_RunningSumOf1DArray {
+	public static void main(String args[]) {
+		int[] arr = new int[] { 1, 2, 3, 4 };
+		System.out.println(Arrays.toString(runningSumOf1DArray(arr)));
+	}
+
+	public static int[] runningSumOf1DArray(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			arr[i] += arr[i - 1];
+		}
+		return arr;
+	}
+}
+//Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+//Return the running sum of nums.
+//Example 1:
+//Input: nums = [1,2,3,4]
+//Output: [1,3,6,10]
+//Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+//Example 2:
+//Input: nums = [1,1,1,1,1]
+//Output: [1,2,3,4,5]
+//Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+//Example 3:
+//Input: nums = [3,1,2,10,1]
+//Output: [3,4,6,16,17]
